1f4fa09eed5bc790e9aa012d5f67ab41f1767cfd
divyanksharma864 <divyanksharma22@gmail.com>
2025-05-01 22:59:21 +0200

    Chef Claude added form

--- session 3 Chef Claude/chef-claude/src/components/Main.jsx
+++ session 3 Chef Claude/chef-claude/src/components/Main.jsx
@@ -1,16 +1,34 @@
 export default function Main(){
+
+    const ingredients = ['Chicken', 'Oregano', 'Tomatoes']
+
+    const ingredientListItems = ingredients.map((ingredient) => (
+        <li key={ingredient}>{ingredient}</li>
+    
+    ))
+
+    function handleSubmit(event){
+        event.preventDefault()
+        const formData = new FormData(event.currentTarget)
+        const newIngredient = formData.get("ingredient")
+    }
+
     return (
         <main>
-            <form className="add-ingredient-form">
+            <form onsubmit={handleSubmit} className="add-ingredient-form">
                 <input
                 type="text"
                 placeholder="e.g. oregano"
                 aria-label="Add ingredient"
+                name="ingredient"
                 />
                 <button>
                     Add Ingredient
                 </button>
             </form>
+            <ul>
+                {ingredientListItems}
+            </ul>
         </main>
     )
 }
